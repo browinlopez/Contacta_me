d6b4925ac5135ad792d4763ee592a89a3257dfaf
browin lopez <browinsantiago@gmail.com>
2022-08-17 15:28:12 -0500

    actualizacion instrucciones

--- screens/instrucciones.js
+++ screens/instrucciones.js
@@ -6,18 +6,19 @@ export default function instrucciones({navigation}) {
     <View style={styles.container}>
       <View style={styles.cont2}>
         <Text style={styles.titulo}>
-            Dependiendo el color del boton que se presiono, será el tiempo en el cual se atendera su solicitud
+        Dependiendo el color del botón que se presionó, será el tiempo en el cual se atenderá su solicitud
         </Text>
-
+      <View>
         <Text style={styles.textbuton2}>
-            <Text style={styles.verde}>Verde:</Text> Si usted preciono el boton verde 
-        </Text>
-        <Text style={styles.textbuton2}>
-        <Text style={styles.amarillo}>Amarillo:</Text> Si usted preciono el boton amarillo se le contestatara de una manera inmediata
+            <Text style={styles.verde}>VERDE:</Text> Si usted presionó el botón verde.
         </Text>
         <Text style={styles.textbuton2}>
-        <Text style={styles.rojo}>Rojo:</Text> Si usted presiona el botón rojo se contactatara a traves de una llamada con uno de nuestros asesores
+        <Text style={styles.amarillo}>AMARILLO:</Text>  Si usted presionó el botón amarillo se le contestataria de una manera inmediata.
         </Text>
+        <Text style={styles.textbuton2}>
+        <Text style={styles.rojo}>ROJO:</Text>Si usted presiona el botón rojo se comunicará a través de una llamada con uno de nuestros asesores.
+        </Text>
+      </View>
       </View>
       <TouchableOpacity style={styles.btnHelp} onPress={()=>navigation.navigate('home')}>
         <Text style={styles.textbuton2}>
@@ -56,29 +57,27 @@ const styles = StyleSheet.create({
         borderEndWidth: 0.5,
         borderTopWidth: 0,
       },
-      textbuton2:{
-        color:'#00000060',
-        fontWeight: 'bold',
-        fontSize: 15
-      },
       titulo:{
-        color:'#9b9b9b',
+        color:'#00000060',
         fontSize:20,
-        fontWeight:'bold'
+        fontWeight:'bold',
+        textAlign: 'center',
+        bottom:30
       },
       textbuton2:{
         color:'#00000060',
         fontWeight: 'bold',
-        fontSize: 15,
-        marginVertical:10
+        fontSize: 13,
+        marginVertical:10,
+        padding:5
       },
       verde:{
-        color:'green'
+        color:'#398e3d'
       },
       amarillo:{
-        color:'yellow'
+        color:'#fec107'
       },
       rojo:{
-        color:'red'
+        color:'#d32f2e'
     }
-}) 
+})
